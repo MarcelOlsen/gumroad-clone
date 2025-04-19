3097fb0a886b4201f2b4229849c44e2cc748352f
MarcelOlsen <marcelolsen26@gmail.com>
2025-04-19 19:54:09 +0200

    Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-4 m-4">
+      <div>
+        <Button variant="elevated">
+          i am a button
+        </Button>
+      </div>
+      <div>
+        <Input
+          placeholder="i am an input"
+        />
+      </div>
+      <div>
+        <Progress
+          value={50}
+        />
+      </div>
+      <div>
+        <Textarea
+          placeholder="i am an input"
+        />
+      </div>
     </div>
   );
 }
